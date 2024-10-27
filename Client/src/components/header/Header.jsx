c0aaadcea48985821aefbb41c0cc871f358cacd4
Abhi <abhiramg563@gmail.com>
2024-10-27 19:57:30 +0530

    fd

--- Client/src/components/header/Header.jsx
+++ Client/src/components/header/Header.jsx
@@ -19,7 +19,7 @@ function Header() {
   <button class="navbar-toggler " type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <div class="collapse navbar-collapse " id="navbarNav">
+  <div class="collapse navbar-collapse cc" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item active">
         <a class="nav-link" href="#"><Link to="" className="nav-link text-dark">
